dd6a6242fcdd3cca48fd91798eed163f3be93110
radin <radinparsaei@gmail.com>
2020-06-14 14:18:33 +0430

    char* -> std::string

--- value.h
+++ value.h
@@ -9,213 +9,28 @@
 #include <string.h>
 #include <sstream>
 #include <iomanip>
-using namespace std;
-#ifndef USE_UTILS
-#endif
-#ifdef USE_UTILS
-#define NUMBER_TO_STRING Utils::stringDuplicate(mpf_class_to_string(this->data.number).c_str());
-#else
-#define NUMBER_TO_STRING stringDuplicate(mpf_class_to_string(this->data.number).c_str());
-#endif
+#define NUMBER_TO_STRING mpf_class_to_string(this->number).c_str();
 #else
 #include <BigNumber.h>
 #define NUMBER BigNumber
-#define NUMBER_TO_STRING data.number.toString()
-#endif
-
-#ifdef USE_UTILS
-namespace Utils {
-	inline size_t stringLength(const char* str) {
-		size_t s = 0;
-		while (str[s] != '\0')s++;
-		return s;
-	}
-
-	inline int find(const char* data, const char* tofind) {
-		size_t c = 0;
-		size_t len = 0;
-		while (data[c] != 0) {
-			if (data[c] == tofind[len]) {
-				len++;
-				if (len == strlen(tofind)) {
-					return c - len + 1;
-				}
-			} else {
-				len = 0;
-			}
-			c++;
-		}
-		return -1;
-	}
-
-	inline char* stringDuplicate(const char* data) {
-		size_t len = strlen(data) + 1;
-		void *dup = malloc(len);
-		return (char*)memcpy(dup, data, len);
-	}
-
-	inline char* freeAndDuplicate(const char* data) {
-		char* res = stringDuplicate(data);
-		delete[] data;
-		return res;
-	}
-
-	inline char* reverse(const char* in) {
-#ifdef USE_UTILS
-			char* data = Utils::stringDuplicate(in);
-#else
-			char* data = stringDuplicate(in);
-#endif
-		size_t len = strlen(data) - 1;
-		for (size_t i = 0; i < len / 2 + 1; i++) {
-			char tmp = *(data + len - i);
-			*(data + len - i) = *(data + i);
-			*(data + i) = tmp;
-		}
-		return data;
-	}
-
-	inline char* replace(const char *replaceOn, const char *from, const char *to) {
-		char* result;
-		int i = 0, c = 0;
-		int tolen = stringLength(to);
-		int fromlen = stringLength(from);
-		for (; replaceOn[i] != '\0'; i++) {
-			if (strstr(&replaceOn[i], from) == &replaceOn[i]) {
-				c++;
-				i += fromlen - 1;
-			}
-		}
-		result = new char[i + c * (tolen - fromlen) + 1];
-		i = 0;
-		while (*replaceOn) {
-			if (strstr(replaceOn, from) == replaceOn) {
-				strcpy(&result[i], to);
-				i += tolen;
-				replaceOn += fromlen;
-			} else {
-				result[i++] = *replaceOn++;
-			}
-		}
-		result[i] = 0;
-		return result;
-	}
-
-	inline char* append(const char* a, const char* b) {
-		size_t alen = strlen(a);
-		size_t len = alen + strlen(b) + 1;
-		char* dup = new char[len];
-		memcpy(dup, a, alen);
-		size_t tmp = 0;
-		while (alen < len) {
-			dup[alen] = *(b + tmp++);
-			alen++;
-		}
-		return dup;
-	}
-
-	inline char* repeat(const char* a, int c) {
-		size_t alen = strlen(a);
-		size_t len = alen * c + 1;
-		char* dup = new char[len];
-		for (size_t i = 0; i < c; i++) memcpy(dup + i * alen, a, alen);
-		dup[len - 1] = 0;
-		return dup;
-	}
-
-	inline bool isEQ(const char* a, const char* b) {
-		size_t i = 0;
-		while (a[i] != 0) {
-			if (b[i] == 0) {
-				return false;
-			}
-			if (a[i] != b[i]) return false;
-			i++;
-		}
-		return a[i] == 0 && b[i] == 0;
-	}
-
-	inline char* substring(const char* on, size_t len, size_t from = 0) {
-		char* sub = (char*)malloc(len + 1);
-		for (size_t i = 0; i < len; i++) {
-			sub[i] = *(on + from + i);
-		}
-		sub[len] = 0;
-		return sub;
-	}
-
-	inline char* rtrim(const char* s) {
-		size_t i = 0;
-		while (s[i] == ' ' || s[i] == '\t' || s[i] == '\n') i++;
-		return substring(s, stringLength(s) - i, i);
-	}
-
-	inline char* ltrim(const char* s) {
-		size_t i = stringLength(s) - 1;
-		while (s[--i] == ' ' || s[i] == '\t' || s[i] == '\n');
-		return substring(s, i + 1);
-	}
-
-	inline char* trim(const char* s) {
-		char* a = rtrim(s);
-		void* pA = a;
-		a = ltrim(a);
-		free(pA);
-		return a;
-	}
-
-	inline char* toUpper(const char* in) {
-		char* s = (char*)(in);
-		size_t size = 0;
-		while (s[size] != 0) {
-			if (s[size] > 'a' && s[size] < 'z') {
-				s[size] -= 32;
-			}
-			size++;
-		}
-		return s;
-	}
-
-	inline char* toLower(const char* in) {
-		char* s = (char*)(in);
-		size_t size = 0;
-		while (s[size] != 0) {
-			if (s[size] > 'A' && s[size] < 'Z') {
-				s[size] += 32;
-			}
-			size++;
-		}
-		return s;
-	}
-}
+#define NUMBER_TO_STRING number.toString()
 #endif
 
 class Value {
-	char* stringDuplicate(const char* data) {
-		size_t len = strlen(data) + 1;
-    char *dup = new char[len];
-    return (char*)memcpy(dup, data, len);
-	}
-	bool type = 0;
-	struct {
+	bool type;
+	NUMBER number;
+	std::string text;
 #ifdef USE_GMP_LIB
-		mpf_class number = 0;
-#else
-		BigNumber number = 0;
-#endif
-		char* string = 0;
-	} data;
-#ifdef USE_GMP_LIB
-	string mpf_class_to_string(mpf_class data) {
-		ostringstream s;
+	std::string mpf_class_to_string(mpf_class data) {
+		std::ostringstream s;
 		int scale = 0;
 		mpf_class tmp = data;
 		while (tmp.get_d() != tmp.get_si()) {
 			tmp /= 10;
 			scale++;
 		}
-	  s << fixed << setprecision(scale) << data;
-	  string str = s.str();
+	  s << std::fixed << std::setprecision(scale) << data;
+	  std::string str = s.str();
 	  int i = str.size();
 	  if (scale) {
 			while(str[--i] == '0')str.pop_back();
@@ -226,28 +41,22 @@ class Value {
 #endif
 	public:
 		Value() { type = 0; }
-		Value(
-#ifdef USE_GMP_LIB
-			mpf_class
-#else
-			BigNumber
-#endif
-			data) {
+		Value(NUMBER data) {
 			type = 0;
-			this->data.number = data;
-			this->data.string = 0;
+			this->number = data;
+			this->text = "";
 		}
 
 		Value(double data) {
 			type = 0;
-			this->data.number = data;
-			this->data.string = 0;
+			this->number = data;
+			this->text = "";
 		}
 
 		Value(int data) {
 			type = 0;
-			this->data.number = data;
-			this->data.string = 0;
+			this->number = data;
+			this->text = "";
 		}
 
 		Value(long long data) {
@@ -263,68 +72,62 @@ class Value {
 				data /= 10;
 			}
 			res[24] = 0;
-			*this =
-#ifdef USE_GMP_LIB
-			mpf_class
-#else
-			BigNumber
-#endif
-			(res);
+			*this = NUMBER(res);
 		}
 
 		Value(long data) {
-			this->data.number = NUMBER(data);
+			this->number = NUMBER(data);
 		}
 
 		Value(const char* data) {
 			type = 1;
-#ifdef USE_UTILS
-			this->data.string = Utils::stringDuplicate(data);
-#else
-			this->data.string = stringDuplicate(data);
-#endif
-			this->data.number = 0;
+			this->text = data;
+			this->number = 0;
+		}
+
+		Value(std::string data) {
+			type = 1;
+			this->text = data;
+			this->number = 0;
 		}
 
 		Value& operator=(const char* data) {
 			type = 1;
-#ifdef USE_UTILS
-			this->data.string = Utils::stringDuplicate(data);
-#else
-			this->data.string = stringDuplicate(data);
-#endif
-			this->data.number = 0;
+			this->text = data;
+			this->number = 0;
+			return *this;
+		}
+
+		Value& operator=(std::string data) {
+			type = 1;
+			this->text = data;
+			this->number = 0;
 			return *this;
 		}
 
 		Value& operator=(Value other) {
 			type = other.type;
-			this->data = other.data;
+			this->text = other.text;
+			this->number = other.number;
 			return *this;
 		}
-		Value& operator=(
-#ifdef USE_GMP_LIB
-			mpf_class
-#else
-			BigNumber
-#endif
-			other) {
+		Value& operator=(NUMBER n) {
 			type = 0;
-			this->data.number = other;
-			this->data.string = 0;
+			this->number = n;
+			this->text = "";
 			return *this;
 		}
 
 		Value& operator=(int i) {
 			type = 0;
-			this->data.number = i;
-			this->data.string = 0;
+			this->number = i;
+			this->text = "";
 			return *this;
 		}
 
-		char* toString() {
+		std::string toString() {
 			if(type){
-				return data.string;
+				return text;
 			} else {
 				return NUMBER_TO_STRING;
 			}
@@ -334,17 +137,12 @@ class Value {
 			return type;
 		}
 
-#ifdef USE_GMP_LIB
-		mpf_class
-#else
-		BigNumber
-#endif
-		getNumber() {
-			return data.number;
+		NUMBER getNumber() {
+			return number;
 		}
 
-		char* getString() {
-			return data.string;
+		std::string getString() {
+			return text;
 		}
 
 		void toNum() {
@@ -352,46 +150,44 @@ class Value {
 			type = 0;
 #ifdef USE_GMP_LIB
 			size_t i = 0;
-			for(; data.string[i] != 0; i++){
-				if(!isdigit(data.string[i])){
-					this->data.number = 0;
-					this->data.string = 0;
+			for(; text[i] != 0; i++){
+				if(!isdigit(text[i])){
+					this->number = 0;
+					this->text = "";
 					return ;
 				}
 			}
 #endif
-			this->data.number = NUMBER(data.string);
-			this->data.string = 0;
+			this->number = NUMBER(text.c_str());
+			this->text = "";
 		}
 
 		void toTxt() {
 			if(type) return;
 			type = 1;
-			this->data.string = NUMBER_TO_STRING;
-			this->data.number = 0;
+			this->text = NUMBER_TO_STRING;
+			this->number = 0;
 		}
 
 		long getLong() {
 #ifdef USE_GMP_LIB
-			return this->data.number.get_si();
+			return this->number.get_si();
 #else
-			return this->data.number.toLong();
+			return this->number.toLong();
 #endif
 		}
 
 		double getDouble() {
 #ifdef USE_GMP_LIB
-			return this->data.number.get_d();
+			return this->number.get_d();
 #else
-			return this->data.number.toDouble();
+			return this->number.toDouble();
 #endif
 		}
 };
 
-#ifdef STD_STREAMS
 std::ostream &operator<<(std::ostream &s, Value &v) {
 		return s << v.toString();
 }
-#endif
 
 #endif
