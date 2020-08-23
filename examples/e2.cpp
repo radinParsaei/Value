9a866d3a8c2cdcc7c25686562c5e0cbb0bd0c80b
radin <radinparsaei@gmail.com>
2020-08-23 22:00:43 +0430

    example e2 added (for bool and null types)

---
+++ examples/e2.cpp
@@ -0,0 +1,16 @@
+#define VALUE_MULTI_TYPE_SUPPORT
+#include <value.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+  Value a = "Hello, World!!!";
+  cout << a << endl;
+  a = 10;
+  cout << a << endl;
+  a = True;
+  cout << a << endl;
+  cout << (a = False) << endl;
+  a = null;
+  cout << a << endl;
+}
