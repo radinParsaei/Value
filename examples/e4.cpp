134207ace9ee676775601a84108f8dd3b92e44c1
radinParsaei <radinparsaei@gmail.com>
2020-09-03 23:24:26 +0430

    example e4 added (a number comparison example)

---
+++ examples/e4.cpp
@@ -0,0 +1,15 @@
+#define NUMBER_COMPARISON_OPERATORS
+#include <value.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+  Value a;
+  cin >> a; //read a text
+  a.toNum(); //convert a to number
+  if (a < 10) {
+    cout << "< 10" << endl;
+  } else {
+    cout << ">= 10" << endl;
+  }
+}
